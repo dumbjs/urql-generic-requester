dfca465cbbc25e322ffcec8065aa55a7bd3a9ccf
Reaper <ahoy@barelyhuman.dev>
2022-01-30 04:26:54 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { DocumentNode } from "graphql";
+import { Client } from "urql";
+
+export const createUrqlRequestor = (client: Client) => {
+  return <Context, Result, Variables extends {}>(
+    document: DocumentNode,
+    params?: Variables,
+    context?: Context
+  ): Promise<Result> => {
+    let op = client.query;
+    const opMeta = document;
+    if (opMeta.kind === "Document") {
+      opMeta.definitions.forEach((def) => {
+        if (
+          def.kind === "OperationDefinition" &&
+          def.operation === "mutation"
+        ) {
+          op = client.mutation;
+        }
+      });
+    }
+    return op(document, params, context)
+      .toPromise()
+      .then((data) => {
+        if (data.error) {
+          return Promise.reject(data.error);
+        }
+        return data.data;
+      });
+  };
+};
